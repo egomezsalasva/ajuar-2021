7f85ea201a0a06d1d101b97a0463072637bc72b6
egomezsalasva <egomezsalasva@gmail.com>
2021-03-26 12:15:27 +0100

    Description responsive desktop, delete gif component

--- components/home/DescriptionHome.js
+++ components/home/DescriptionHome.js
@@ -2,7 +2,7 @@
     // - Modules
     import styled from 'styled-components'
     // - Components
-    import DescriptionGif from './DescriptionGif.js'
+    //import DescriptionGif from './DescriptionGif.js'
     // - Styles
     import { brandingColors, brandingFonts } from '../../styles/customStyles/brandingStyles'
     import { breakpoints, pxToVW } from '../../styles/customStyles/globalStyles'
@@ -26,13 +26,14 @@
         }
     `
     const Line = styled.div`
-        //font-size: 3.5rem;
         font-size: 3.64vw;
         line-height: 1.13;
         letter-spacing: 0.5px;
         color: ${brandingColors.light};
+        @media (max-width: ${breakpoints.laptopDesign}) {
+            font-size: 3.5rem;
+        }
         @media (max-width: ${breakpoints.tabletDesign}) {
-            font-size: 3.68vw;
             font-size: 2.4rem;
         }
         @media (max-width: ${breakpoints.mobileDesign}) {
@@ -41,27 +42,24 @@
         }
 
         .emoji{
-            //font-size: 3rem;
             font-size: 3.125vw;
-            @media (max-width: ${breakpoints.tabletDesign}) {
-                font-size: 3.125vw;
-                font-size: 2.1rem;
+            @media (max-width: ${breakpoints.laptopDesign}) {
+                font-size: 3rem;
             }
-            @media (max-width: ${breakpoints.mobileDesign}) {
+            @media (max-width: ${breakpoints.tabletDesign}) {
                 font-size: 2.1rem;
             }
         }
 
         .supScript{
             font-family: ${brandingFonts.display};
-            //font-size: 2rem;
             font-size: 2.083vw;
             vertical-align: text-top;
-            @media (max-width: ${breakpoints.tabletDesign}) {
-                //font-size: 2.083vw;
-                //font-size: 1.2rem;
+
+            @media (max-width: ${breakpoints.laptopDesign}) {
+                font-size: 2rem;
             }
-            @media (max-width: ${breakpoints.mobileDesign}) {
+            @media (max-width: ${breakpoints.tabletDesign}) {
                 font-size: 1.2rem;
             }
         }
@@ -73,11 +71,13 @@
     export default function DescriptionHome() {
         return (
             <DescriptionContainer>
-                <Line><span className="emoji">👇🏽</span> <br/>
-                SOMOS AJUAR <span className="supScript">(AJUAR)</span>,<br/>
-                EL MENAJE DIGITAL <span className="emoji">🤳🏽</span> MULTIDISCIPLINAR<br/>
-                QUE NECESITA TU PROYECTO <span className="emoji">🏡</span><br/>
-                PARA EL SI <span className="emoji">💍</span> QUIERO DE TU CLIENTE <span className="supScript">(AMOR)</span>.</Line>
+                <Line>
+                    <span className="emoji">👇🏽</span> <br/>
+                    SOMOS AJUAR <span className="supScript">(AJUAR)</span>,<br/>
+                    EL MENAJE DIGITAL <span className="emoji">🤳🏽</span> MULTIDISCIPLINAR<br/>
+                    QUE NECESITA TU PROYECTO <span className="emoji">🏡</span><br/>
+                    PARA EL SI <span className="emoji">💍</span> QUIERO DE TU CLIENTE <span className="supScript">(AMOR)</span>.
+                </Line>
             </DescriptionContainer>
         )
     }
